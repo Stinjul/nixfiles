0d8162ec5cdba6820ea33aa179263ecf5561b3a8
Stinjul <3649882+Stinjul@users.noreply.github.com>
2023-10-21 00:36:55 +0200

    Migrate ksops to KRM style install

--- pkgs/ksops.nix
+++ pkgs/ksops.nix
@@ -13,24 +13,15 @@ buildGoModule rec {
 
   vendorHash = "sha256-IoQ3Hyyhk0jsEPyEajRa7HQrgV6ykLCj9vjTIQYOQtw=";
 
-  installPhase = ''
-    mkdir -p $out/lib/viaduct.ai/v1/ksops/
-    mkdir -p $out/lib/viaduct.ai/v1/ksops-exec/
-    mv $GOPATH/bin/kustomize-sops $out/lib/viaduct.ai/v1/ksops/ksops
-    ln -s $out/lib/viaduct.ai/v1/ksops/ksops $out/lib/viaduct.ai/v1/ksops-exec/ksops-exec
-  '';
-
-  # Tests are broken in a nix environment
   doCheck = false;
+
+  postBuild = ''
+    mv $GOPATH/bin/kustomize-sops $GOPATH/bin/ksops
+  '';
 
   meta = with lib; {
     description = "A Flexible Kustomize Plugin for SOPS Encrypted Resource";
-    longDescription = ''
-      KSOPS can be used to decrypt any Kubernetes resource, but is most commonly
-      used to decrypt encrypted Kubernetes Secrets and ConfigMaps.
-    '';
     homepage = "https://github.com/viaduct-ai/kustomize-sops";
     license = licenses.asl20;
-    maintainers = with maintainers; [ starcraft66 ];
   };
 }
