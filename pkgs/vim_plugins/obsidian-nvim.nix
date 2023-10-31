58d13cefa602375d3e81839a83f0bca341eedd5a
Stinjul <3649882+Stinjul@users.noreply.github.com>
2023-10-31 16:38:40 +0100

    Fix obsidian typo

--- pkgs/vim_plugins/obsidian-nvim.nix
+++ pkgs/vim_plugins/obsidian-nvim.nix
@@ -4,7 +4,7 @@ vimUtils.buildVimPlugin {
   version = "v1.15.0";
   src = fetchFromGitHub {
     owner = "epwalsh";
-    repo = "obisidan.nvim";
+    repo = "obsidian.nvim";
     rev = "630c92045f0595123031b598c624dbad3b5f6dbc";
     sha256 = "0irnwlwpsr9dnq87dyvvz00rwzmqp6i0sc2n3h04b6nkqpgskdgz";
   };
