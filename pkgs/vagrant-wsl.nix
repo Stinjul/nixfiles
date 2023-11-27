109493a468aa53043e04a2feaec102e55b5c6d5a
Stinjul <3649882+Stinjul@users.noreply.github.com>
2023-11-27 22:08:55 +0100

    Add windows path stuff to vagrant-wsl

--- pkgs/vagrant-wsl.nix
+++ pkgs/vagrant-wsl.nix
@@ -4,6 +4,6 @@
 
   buildInputs = [ pkgs.makeWrapper ];
   postBuild = ''
-    wrapProgram $out/bin/vagrant --set VAGRANT_WSL_ENABLE_WINDOWS_ACCESS 1
+    wrapProgram $out/bin/vagrant --set VAGRANT_WSL_ENABLE_WINDOWS_ACCESS 1 --set PATH "\$PATH:/mnt/c/Windows/SysWOW64/:/mnt/c/Program Files/PowerShell/7/"
   '';
 }
