092154a17bfbd80450d834a5a5a67981b7ad7ff5
Stinjul <3649882+Stinjul@users.noreply.github.com>
2023-11-28 04:22:36 +0100

    Vagrant-wsl hyperv fixes

--- pkgs/vagrant-wsl.nix
+++ pkgs/vagrant-wsl.nix
@@ -4,6 +4,8 @@
 
   buildInputs = [ pkgs.makeWrapper ];
   postBuild = ''
-    wrapProgram $out/bin/vagrant --set VAGRANT_WSL_ENABLE_WINDOWS_ACCESS 1 --set PATH "\$PATH:/mnt/c/Windows/SysWOW64/:/mnt/c/Program Files/PowerShell/7/"
+    wrapProgram $out/bin/vagrant --set VAGRANT_WSL_ENABLE_WINDOWS_ACCESS 1 \
+    --set PATH "\$PATH:/mnt/c/Windows/SysWOW64/:/mnt/c/Program Files/PowerShell/7/:/mnt/c/Windows/SysWOW64/WindowsPowerShell/v1.0/" \
+    --set VAGRANT_DEFAULT_PROVIDER="hyperv"
   '';
 }
