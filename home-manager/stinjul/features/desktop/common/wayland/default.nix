53a0f9b2d519c6a77c17af50cb5afc18ed124751
Stinjul <3649882+Stinjul@users.noreply.github.com>
2024-03-14 21:32:23 +0100

    Add wayvnc

--- home-manager/stinjul/features/desktop/common/wayland/default.nix
+++ home-manager/stinjul/features/desktop/common/wayland/default.nix
@@ -1,7 +1,10 @@
-{
+{ pkgs, ... }: {
   imports = [
     ./kitty.nix
     ./rofi.nix
+  ];
+  home.packages = with pkgs; [
+    wayvnc
   ];
   home.sessionVariables = {
     MOZ_ENABLE_WAYLAND = 1;
