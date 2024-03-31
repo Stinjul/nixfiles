4a329391a215dc52ff158ba20fe7733d6b6995be
Stinjul <4914129-Stinjul@users.noreply.gitlab.com>
2024-03-31 01:46:05 +0100

    Add gtk config

--- home-manager/stinjul/features/desktop/common/gtk.nix
+++ home-manager/stinjul/features/desktop/common/gtk.nix
@@ -1,5 +1,25 @@
+{ pkgs, ... }:
+# home.packages = with pkgs; [
+#   gnome.adwaita-icon-theme
+# ];
+let
+  theme = {
+    name = "adw-gtk3-dark";
+    package = pkgs.adw-gtk3;
+  };
+in
 {
+  home = {
+    file.".local/share/themes/${theme.name}" = {
+      source = "${theme.package}/share/themes/${theme.name}";
+    };
+  };
   gtk = {
+    inherit theme;
     enable = true;
+    iconTheme = {
+      name = "Adwaita";
+      package = pkgs.gnome.adwaita-icon-theme;
+    };
   };
 }
