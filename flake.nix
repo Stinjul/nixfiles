b2a6bbb96cf1de00cb1f71e8901580ca36892fab
Stinjul <3649882+Stinjul@users.noreply.github.com>
2024-04-25 11:46:24 +0200

    Flake bump

--- flake.nix
+++ flake.nix
@@ -20,6 +20,7 @@
     nixvim = {
       url = "github:nix-community/nixvim";
       inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
     };
 
     hardware.url = "github:nixos/nixos-hardware";
