e01210ed8ae0eb4deeaeff3a561ea81266be455e
Stinjul <3649882+Stinjul@users.noreply.github.com>
2024-05-26 15:11:29 +0200

    Add persistence to kdeconnect

--- home-manager/stinjul/features/desktop/common/kdeconnect.nix
+++ home-manager/stinjul/features/desktop/common/kdeconnect.nix
@@ -1,6 +1,9 @@
-{
+{config, ...}: {
     services.kdeconnect = {
         enable = true;
         indicator = true;
+    };
+    home.persistence = {
+      "/persist${config.home.homeDirectory}".directories = [".config/kdeconnect"];
     };
 }
