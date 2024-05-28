98ee21a581ccb3d3ef96123bb4a8a6ff6fdedb2f
Stinjul <3649882+Stinjul@users.noreply.github.com>
2024-05-29 00:37:36 +0200

    Move root and make it non-persistent

--- hosts/zennix/hardware-configuration.nix
+++ hosts/zennix/hardware-configuration.nix
@@ -15,9 +15,36 @@
   };
 
   fileSystems."/" = {
-    device = "/dev/disk/by-uuid/32b70672-644c-4292-8781-e0d24bb482cb";
+    # device = "/dev/disk/by-uuid/32b70672-644c-4292-8781-e0d24bb482cb";
+    device = "/dev/disk/by-uuid/a5983d64-30fe-4d37-918a-8bd9e7f5fe15";
     fsType = "btrfs";
+    options = [ "subvol=root" ];
   };
+
+  boot.initrd.postDeviceCommands = lib.mkAfter ''
+    mkdir /btrfs_tmp
+    mount /dev/disk/by-uuid/a5983d64-30fe-4d37-918a-8bd9e7f5fe15 /btrfs_tmp
+    if [[ -e /btrfs_tmp/root ]]; then
+        mkdir -p /btrfs_tmp/old_roots
+        timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
+        mv /btrfs_tmp/root "/btrfs_tmp/old_roots/$timestamp"
+    fi
+
+    delete_subvolume_recursively() {
+        IFS=$'\n'
+        for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
+            delete_subvolume_recursively "/btrfs_tmp/$i"
+        done
+        btrfs subvolume delete "$1"
+    }
+
+    for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +30); do
+        delete_subvolume_recursively "$i"
+    done
+
+    btrfs subvolume create /btrfs_tmp/root
+    umount /btrfs_tmp
+  '';
 
   fileSystems."/boot" = {
     device = "/dev/disk/by-uuid/F2BF-33D5";
