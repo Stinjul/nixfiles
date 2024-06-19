5846cc051da90267c7a480192369136a57eb7886
Stinjul <3649882+Stinjul@users.noreply.github.com>
2024-06-19 23:03:49 +0200

    Add more ram to starsector and fix settings

--- home-manager/stinjul/features/desktop/common/starsector.nix
+++ home-manager/stinjul/features/desktop/common/starsector.nix
@@ -1,17 +1,24 @@
 { pkgs, config, ... }: {
   home = {
     packages = [
-      pkgs.starsector
+      (pkgs.starsector.overrideAttrs (old: {
+        postPatch = old.postPatch + ''
+          substituteInPlace starsector.sh \
+            --replace-fail "-Xms1536m -Xmx1536m" "-Xms4096m -Xmx4096m"
+        '';
+      }))
     ];
     persistence = {
       "/persist${config.home.homeDirectory}" = {
         directories = [
           # { directory = ".local/share/starsector"; method = "symlink"; }
           ".local/share/starsector"
+          ".java/.userPrefs/com/fs/starfarer"
         ];
-        files = [
-          ".java/.userPrefs/com/fs/starfarer/prefs.xml"
-        ];
+        # Starsector keeps copying/overwriting the symlink :(
+        # files = [
+        #   ".java/.userPrefs/com/fs/starfarer/prefs.xml"
+        # ];
       };
     };
   };
