1c6ec629612ae9627ed58600592723ef62cfe0ab
Stinjul <3649882+Stinjul@users.noreply.github.com>
2024-07-09 00:26:02 +0200

    Add other mgmt-nucs

--- hosts/homelab/mgmt-nuc-2.nix
+++ hosts/homelab/mgmt-nuc-2.nix
@@ -6,10 +6,10 @@
   services.k3s.serverAddr = "https://172.16.10.5:6443";
   networking = {
     hostName = "mgmt-nuc-2";
-    interfaces.enp86s0.ipv4.addresses = [{
-      address = "172.16.10.6";
-      prefixLength = 24;
-    }];
+    # interfaces.enp86s0.ipv4.addresses = [{
+    #   address = "172.16.10.6";
+    #   prefixLength = 24;
+    # }];
   };
 }
 
--- hosts/homelab/mgmt-nuc-3.nix
+++ hosts/homelab/mgmt-nuc-3.nix
@@ -6,10 +6,10 @@
   services.k3s.serverAddr = "https://172.16.10.5:6443";
   networking = {
     hostName = "mgmt-nuc-3";
-    interfaces.enp86s0.ipv4.addresses = [{
-      address = "172.16.10.7";
-      prefixLength = 24;
-    }];
+    # interfaces.enp86s0.ipv4.addresses = [{
+    #   address = "172.16.10.7";
+    #   prefixLength = 24;
+    # }];
   };
 }
 
