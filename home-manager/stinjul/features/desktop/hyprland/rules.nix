75e5ebb0de72baabcc45a142c1694f9c3efe8e27
Stinjul <3649882+Stinjul@users.noreply.github.com>
2024-07-12 16:37:14 +0200

    Stop steam from stealing focus >:(

--- home-manager/stinjul/features/desktop/hyprland/rules.nix
+++ home-manager/stinjul/features/desktop/hyprland/rules.nix
@@ -1,19 +1,20 @@
-{ config, ... }: {
+{ config, ... }:
+{
   wayland.windowManager.hyprland.settings = {
     windowrulev2 = [
       "workspace 4,class:firefox"
       "workspace 5,class:(i?)libreoffice.*"
       "workspace 7,class:^steam_app_.*"
       "workspace 7,title:.*(i?)wine.*"
-      "workspace 8,class:^[Ss]team$"
+      "workspace 8 silent,class:^[Ss]team$"
       "workspace 9,class:^discord$"
     ];
-    workspace = 
-    let
+    workspace =
+      let
         terminal = config.home.sessionVariables.TERMINAL;
-    in
-    [
+      in
+      [
         "special:scratchpad, gapsout:20, on-created-empty:${terminal} --class dropdown --session ~/.config/kitty/scratchpad.session --override background_opacity=1"
-    ];
+      ];
   };
 }
