9cc71955e7adb1e31f8b4c8168f688d3915664a8
Stinjul <3649882+Stinjul@users.noreply.github.com>
2024-11-29 17:21:10 +0100

    Fix broken bibata hyprcursor pkg (How did I miss this)

--- pkgs/bibata-hyprcursor/default.nix
+++ pkgs/bibata-hyprcursor/default.nix
@@ -1,85 +1,105 @@
 # https://github.com/diniamo/niqspkgs/blob/main/pkgs/bibata-hyprcursor/default.nix
 {
+  lib,
   stdenvNoCC,
   fetchFromGitHub,
   python3,
   python3Packages,
-  lib,
   hyprcursor,
   variant ? "modern",
   baseColor ? "#000000",
   outlineColor ? "#FFFFFF",
   watchBackgroundColor ? "#000000",
+  colorName ? "classic",
 }:
-assert builtins.elem variant ["modern" "modern-right" "original" "original-right"];
-  stdenvNoCC.mkDerivation (final: {
-    pname = "bibata-hyprcursor";
-    version = "v2.0.7";
+let
+  capitalize =
+    str:
+    let
+      capital_letter = builtins.substring 0 1 str;
+      non_capital = lib.removePrefix capital_letter str;
+    in
+    lib.toUpper capital_letter + non_capital;
 
-    src = fetchFromGitHub {
-      owner = "ful1e5";
-      repo = "Bibata_Cursor";
-      rev = final.version;
-      hash = "sha256-kIKidw1vditpuxO1gVuZeUPdWBzkiksO/q2R/+DUdEc=";
-    };
+  themeName = "Bibata-${capitalize variant}-${capitalize colorName}-Hyprcursor";
+in
+assert builtins.elem variant [
+  "modern"
+  "modern-right"
+  "original"
+  "original-right"
+];
+stdenvNoCC.mkDerivation (final: {
+  pname = "bibata-hyprcursor";
+  version = "v2.0.7";
 
-    nativeBuildInputs = [
-      python3
-      python3Packages.tomli
-      python3Packages.tomli-w
-      hyprcursor.out
-    ];
+  src = fetchFromGitHub {
+    owner = "ful1e5";
+    repo = "Bibata_Cursor";
+    rev = final.version;
+    hash = "sha256-kIKidw1vditpuxO1gVuZeUPdWBzkiksO/q2R/+DUdEc=";
+  };
 
-    phases = ["unpackPhase" "configurePhase" "buildPhase" "installPhase"];
+  nativeBuildInputs = [
+    python3
+    python3Packages.tomli
+    python3Packages.tomli-w
+    hyprcursor
+  ];
 
-    unpackPhase = ''
-      runHook preUnpack
+  phases = [
+    "unpackPhase"
+    "configurePhase"
+    "buildPhase"
+    "installPhase"
+  ];
 
-      cp $src/configs/${
-        if lib.hasSuffix "right" variant
-        then "right"
-        else "normal"
-      }/x.build.toml config.toml
+  unpackPhase = ''
+    runHook preUnpack
 
-      mkdir cursors
-      for cursor in $src/svg/${variant}/*; do
-        cp -r $src/svg/${variant}/$(readlink $cursor) cursors
-      done
+    cp $src/configs/${
+      if lib.hasSuffix "right" variant then "right" else "normal"
+    }/x.build.toml config.toml
 
-      chmod -R u+w .
+    mkdir cursors
+    for cursor in $src/svg/${variant}/*; do
+      cp -r $src/svg/${variant}/$(readlink $cursor) cursors
+    done
 
-      runHook postUnpack
-    '';
+    chmod -R u+w .
 
-    configurePhase = ''
-      runHook preConfigure
+    runHook postUnpack
+  '';
 
-      cat << EOF > manifest.hl
-      name = Bibata-${variant}
-      description = The Bibata Cursor theme packaged for hyprcursor.
-      version = ${final.version}
-      cursors_directory = cursors
-      EOF
+  configurePhase = ''
+    runHook preConfigure
 
-      find cursors -type f -name '*.svg' | xargs sed -i -e 's/#00FF00/${baseColor}/g' -e 's/#0000FF/${outlineColor}/g' -e 's/#FF0000/${watchBackgroundColor}/g'
+    cat << EOF > manifest.hl
+    name = ${themeName}
+    description = The Bibata Cursor theme packaged for hyprcursor.
+    version = ${final.version}
+    cursors_directory = cursors
+    EOF
 
-      python ${./configure.py} config.toml cursors
+    find cursors -type f -name '*.svg' | xargs sed -i -e 's/#00FF00/${baseColor}/g' -e 's/#0000FF/${outlineColor}/g' -e 's/#FF0000/${watchBackgroundColor}/g'
 
-      runHook postConfigure
-    '';
+    python ${./configure.py} config.toml cursors
 
-    buildPhase = ''
-      runHook preBuild
-      hyprcursor-util --create . --output .
-      runHook postBuild
-    '';
+    runHook postConfigure
+  '';
 
-    installPhase = ''
-      runHook preInstall
+  buildPhase = ''
+    runHook preBuild
+    hyprcursor-util --create . --output .
+    runHook postBuild
+  '';
 
-      mkdir -p $out/share/icons
-      cp -r theme_Bibata-${variant} $out/share/icons/Bibata-${variant}
+  installPhase = ''
+    runHook preInstall
 
-      runHook postInstall
-    '';
-  })
+    mkdir -p $out/share/icons
+    cp -r theme_${themeName} $out/share/icons/${themeName}
+
+    runHook postInstall
+  '';
+})
