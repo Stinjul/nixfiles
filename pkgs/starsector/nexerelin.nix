fc32cc2cbf7ff7c6627e47c55fd11d6302123e5e
Stinjul <3649882+Stinjul@users.noreply.github.com>
2024-12-04 16:31:23 +0100

    Update and fix starsector nexerelin url

--- pkgs/starsector/nexerelin.nix
+++ pkgs/starsector/nexerelin.nix
@@ -4,10 +4,10 @@
 }:
 buildStarsectorMod rec {
   pname = "nexerelin";
-  version = "0.11.2c";
+  version = "0.11.3";
 
   src = fetchzip {
-    url = "https://github.com/Histidine91/Nexerelin/releases/download/v0.11.2c/Nexerelin_${version}.zip";
-    sha256 = "sha256-cYdlzsDH74PH4mhuYDfqsZJGje8T1e5NvhKF/T8D/os=";
+    url = "https://github.com/Histidine91/Nexerelin/releases/download/v${version}/Nexerelin_${version}.zip";
+    sha256 = "sha256-erY4o+kalMzSiBhE9Pt380k8ndvgrCDWGQ9FhWAgcZE=";
   };
 }
