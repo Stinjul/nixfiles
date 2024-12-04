3156d429d00d61d9e51bfa12228945defddfa263
Stinjul <3649882+Stinjul@users.noreply.github.com>
2024-12-04 02:10:49 +0100

    Remove logrotate.status from mgmt-nuc persistence

--- hosts/homelab/mgmt-nuc/default.nix
+++ hosts/homelab/mgmt-nuc/default.nix
@@ -92,7 +92,10 @@
   };
 
   environment.persistence."/persist" = {
-    files = [ "/var/lib/logrotate.status" ];
+    # I've seen people put logrotate in this but it wants to rename this file while updating it
+    # Which doesn't work when it's a bind mount it seems
+    # Don't think it's that important anyway, logs get killed after each reboot
+    # files = [ "/var/lib/logrotate.status" ];
     directories = [
       "/etc/rancher"
       "/var/lib/rancher"
