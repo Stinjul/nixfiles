a3c000ef3c13199c9cbf3f471085f136a0c15a42
Stinjul <3649882+Stinjul@users.noreply.github.com>
2025-01-19 19:38:12 +0100

    Update dns settings for zennix

--- hosts/zennix/dns.nix
+++ hosts/zennix/dns.nix
@@ -1,14 +1,15 @@
 {
-    services.dnsmasq = {
-        enable = true;
-        resolveLocalQueries = true;
-        settings = {
-            address = [ 
-              "/stinjul.com/192.168.1.59" 
-            ];
-            server = [ 
-              "/kube.k3s-mgmt.stinjul.com/172.16.200.1" 
-            ];
-        };
+  services.dnsmasq = {
+    enable = true;
+    resolveLocalQueries = true;
+    settings = {
+      # address = [
+      #   "/stinjul.com/192.168.1.59"
+      # ];
+      server = [
+        "/mgmt.stinjul.com/172.16.200.1"
+        "/kube.k3s-mgmt.stinjul.com/172.16.200.1"
+      ];
     };
+  };
 }
