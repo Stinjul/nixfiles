1b37fbf1270174bf11e2054832528a5b92ea7067
Stinjul <3649882+Stinjul@users.noreply.github.com>
2025-01-21 21:27:38 +0100

    Add yazi git plugin

--- home-manager/stinjul/features/cli/yazi.nix
+++ home-manager/stinjul/features/cli/yazi.nix
@@ -1,14 +1,46 @@
-{
-    programs.yazi = {
-        enable = true;
-        keymap = {
-            manager.prepend_keymap = [
-                {
-                    on = "!";
-                    run = "shell \"$SHELL\" --block";
-                    desc = "open shell here";
-                }
-            ];
-        };
+{ pkgs, ... }:
+let
+  plugins = {
+    yazi = pkgs.fetchFromGitHub {
+      owner = "yazi-rs";
+      repo = "plugins";
+      rev = "7707c09f03e02144e528625ba82f54c6177715b2";
+      hash = "sha256-oWdptgAtTAHX5u7lYe1o2TlvyCiOsuV1D5gm85J4dSE=";
     };
+  };
+
+in
+{
+  programs.yazi = {
+    enable = true;
+    plugins = {
+      git = "${plugins.yazi}/git.yazi";
+    };
+    initLua = ''
+      require("git"):setup()
+    '';
+    settings = {
+      plugin.prepend_fetchers = [
+        {
+          id = "git";
+          name = "*";
+          run = "git";
+        }
+        {
+          id = "git";
+          name = "*/";
+          run = "git";
+        }
+      ];
+    };
+    keymap = {
+      manager.prepend_keymap = [
+        {
+          on = "!";
+          run = "shell \"$SHELL\" --block";
+          desc = "open shell here";
+        }
+      ];
+    };
+  };
 }
