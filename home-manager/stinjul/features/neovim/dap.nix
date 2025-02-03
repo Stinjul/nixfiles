d5dae4f5738dd6d4029b34bd44fc779fe1d04eaf
Stinjul <3649882+Stinjul@users.noreply.github.com>
2025-02-03 19:44:08 +0100

    Bump flakes, update renamed options

--- home-manager/stinjul/features/neovim/dap.nix
+++ home-manager/stinjul/features/neovim/dap.nix
@@ -1,63 +1,74 @@
-{ pkgs, ... }: {
-  programs.nixvim = { helpers, ... }: {
-    extraPackages = with pkgs; [
-      lldb
-      delve
-    ];
+{ pkgs, ... }:
+{
+  programs.nixvim =
+    { helpers, ... }:
+    {
+      extraPackages = with pkgs; [
+        lldb
+        delve
+      ];
 
-    plugins.dap = {
-      enable = true;
-      extensions = {
+      plugins = {
         dap-ui.enable = true;
         dap-go.enable = true;
         dap-python.enable = true;
+        dap-lldb.enable = true;
       };
-      adapters.executables = {
-        lldb = {
-          command = "lldb-dap";
-        };
-        elixir = {
-          command = "elixir-debug-adapter";
-        };
-      };
-      configurations =
-        let
-          lldbLangs = [ "c" "cpp" ];
-        in
-        builtins.listToAttrs
-          (map
-            (lang: {
-              name = lang;
-              value = [
-                {
-                  name = "Launch";
-                  type = "lldb";
-                  request = "launch";
-                  cwd = "\${workspaceFolder}";
-                  program = helpers.mkRaw ''
-                    function()
-                      return vim.fn.input('Executable path: ', vim.fn.getcwd() .. '/', 'file')
-                    end
-                  '';
-                }
-              ];
-            })
-            lldbLangs) // {
-          elixir = [{
-            name = "mix test";
-            type = "elixir";
-            task = "test";
-            taskArgs = [ "--trace" ];
-            request = "launch";
-            startApps = true; # for Phoenix projects
-            projectDir = "\${workspaceFolder}";
-            requireFiles = [
-              "test/**/test_helper.exs"
-              "test/**/*_test.exs"
-            ];
-          }];
-        };
 
+      plugins.dap = {
+        enable = true;
+        adapters.executables = {
+          # lldb = {
+          #   command = "lldb-dap";
+          # };
+          elixir = {
+            command = "elixir-debug-adapter";
+          };
+        };
+        configurations =
+          # let
+          #   lldbLangs = [
+          #     "c"
+          #     "cpp"
+          #   ];
+          # in
+          # builtins.listToAttrs (
+          #   map (lang: {
+          #     name = lang;
+          #     value = [
+          #       {
+          #         name = "Launch";
+          #         type = "lldb";
+          #         request = "launch";
+          #         cwd = "\${workspaceFolder}";
+          #         program = helpers.mkRaw ''
+          #           function()
+          #             return vim.fn.input('Executable path: ', vim.fn.getcwd() .. '/', 'file')
+          #           end
+          #         '';
+          #       }
+          #     ];
+          #   }) lldbLangs
+          # )
+          # //
+          {
+            elixir = [
+              {
+                name = "mix test";
+                type = "elixir";
+                task = "test";
+                taskArgs = [ "--trace" ];
+                request = "launch";
+                startApps = true; # for Phoenix projects
+                projectDir = "\${workspaceFolder}";
+                requireFiles = [
+                  "test/**/test_helper.exs"
+                  "test/**/*_test.exs"
+                ];
+              }
+            ];
+          };
+
+      };
     };
-  };
 }
