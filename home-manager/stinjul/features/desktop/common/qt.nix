9e3e8583b62816e68b0c9dd5ef9a07bd5e0730f7
Stinjul <3649882+Stinjul@users.noreply.github.com>
2025-07-12 00:26:11 +0200

    Fix qt theming

--- home-manager/stinjul/features/desktop/common/qt.nix
+++ home-manager/stinjul/features/desktop/common/qt.nix
@@ -1,5 +1,12 @@
-{
+{config, ...}: {
+  home = {
+    persistence = {
+      "/persist${config.home.homeDirectory}".directories = [ ".config/qt5ct" ".config/qt6ct" ];
+    };
+  };
   qt = {
     enable = true;
+    platformTheme.name = "qtct";
+    # style.name = "breeze";
   };
 }
