c384c207b6504d80c983726d6ac06dd38de71555
Stinjul <3649882+Stinjul@users.noreply.github.com>
2025-09-22 19:49:08 +0200

    Bump flakes

--- home-manager/stinjul/features/desktop/common/wayland/rofi.nix
+++ home-manager/stinjul/features/desktop/common/wayland/rofi.nix
@@ -1,7 +1,6 @@
 { config, lib, pkgs, ... }: {
   programs.rofi = {
     enable = true;
-    package  = pkgs.rofi-wayland;
     extraConfig = {
         run-command = "fish -c '{cmd}'";
         run-list-command = "fish -c functions";
