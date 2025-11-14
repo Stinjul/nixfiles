beb18061f55c0406e878bc6374ccef615f5f8467
Stinjul <3649882+Stinjul@users.noreply.github.com>
2025-11-15 00:17:49 +0100

    add symlink to yazi statusbar

--- home-manager/stinjul/features/cli/yazi.lua
+++ home-manager/stinjul/features/cli/yazi.lua
@@ -13,3 +13,12 @@ Status:children_add(function()
         " ",
     }
 end, 500, Status.RIGHT)
+
+Status:children_add(function(self)
+    local h = self._current.hovered
+    if h and h.link_to then
+        return " -> " .. tostring(h.link_to)
+    else
+        return ""
+    end
+end, 3300, Status.LEFT)
