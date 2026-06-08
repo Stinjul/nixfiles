0028213c5300019990ba48a813cd4f4821a427f1
Stinjul <3649882+Stinjul@users.noreply.github.com>
2026-06-08 22:17:51 +0200

    build(deps): bump flakes

--- home-manager/stinjul/features/cli/yazi.nix
+++ home-manager/stinjul/features/cli/yazi.nix
@@ -25,12 +25,12 @@
     settings = {
       plugin.prepend_fetchers = [
         {
-          id = "git";
+          group = "git";
           url = "*";
           run = "git";
         }
         {
-          id = "git";
+          group = "git";
           url = "*/";
           run = "git";
         }
--- home-manager/stinjul/features/neovim/default.nix
+++ home-manager/stinjul/features/neovim/default.nix
@@ -23,6 +23,7 @@
 
   programs.nixvim = {
     enable = true;
+    nixpkgs.useGlobalPackages = true;
 
     colorschemes.base16 = {
       enable = true;
